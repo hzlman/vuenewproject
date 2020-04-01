d751c8235cffc7327832a50d4306123093dee2f6
Your Name <1326145662@qq.com>
2020-04-01 14:58:16 +0800

    first commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-04-01 01:24:42
+ * @LastEditTime: 2020-04-01 14:56:27
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \vueObject\vuenewproject\README.md
+ -->
+
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2020-04-01 01:24:42
+ * @LastEditTime: 2020-04-01 01:53:08
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \vueObject\vuenewproject\src\router\index.js
+ */
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: []
+})
